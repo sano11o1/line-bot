cfcd5e87e0ffdab3b88bd48604edad692595f0be
sano11o1 <mashio.tech@gmail.com>
2021-11-23 22:14:11 +0900

    ユーザーを登録を送るとユーザーレコードを作成する

--- app/controllers/line_webhook_controller.rb
+++ app/controllers/line_webhook_controller.rb
@@ -4,10 +4,21 @@ class LineWebhookController < ApplicationController
   before_action :validate_signature
   
   def create
-    event = @line_client.parse_events_from(request.body)[0]
-    if event.message.text == 'ユーザー登録'
-      response = get_profile(event.source.userId)
-      User.create(name: response.displayName, line_user_id: response.userId)
+    events = params['events']
+    if !events.empty?
+      user_id = params['events'][0]['source']['userId']
+      if events[0]['message']['text'] == 'ユーザー登録'
+        response = @line_client.get_profile(user_id)
+        parsed_body= JSON.parse(response.body)
+        if User.find_by(line_user_id: user_id)
+          @line_client.reply_message(events[0]['replyToken'], {type: 'text', text: 'ユーザー登録済みです'})
+        else
+          User.create(name: parsed_body['displayName'], line_user_id: user_id)
+          @line_client.reply_message(events[0]['replyToken'], {type: 'text', text: 'ユーザーを作成しました！'})
+        end
+      else
+        @line_client.reply_message(events[0]['replyToken'], {type: 'text', text: 'そのメッセージには応答できません'})
+      end
     end
     render status: 200, json: { status: 200, message: 'OK Ranch!'}
   end
